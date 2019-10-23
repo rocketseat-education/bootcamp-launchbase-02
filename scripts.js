3b6a6a122637a53cb86c139d025e7d795cedb9e0
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-10-22 21:42:38 -0300

    08 - Iframe Video Dinamico

--- scripts.js
+++ scripts.js
@@ -3,10 +3,14 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id");
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function() {
     modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = ""
 })
+
